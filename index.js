8b169becb9d1a3c037ea65e1907ed70b83bcbee7
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-03-09 19:26:36 -0500

    Complet lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let messages = [];
+
+function writeCards(stringNames,eventNames){
+   
+    for(let i = 0 ; i<stringNames.length; i++){
+        messages.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventNames} gift!`)
+
+    }    
+    return messages;
+}
+
+console.log(messages);
+
+function countDown(count){
+   
+    while(count > 0 || count ==0){
+        console.log(count);
+        count--;
+    }
+    return count;
+}
